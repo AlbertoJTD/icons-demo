887e665aabb75082eb4ec171d514023affe2c6d5
AlbertoJTD <albertojtd@outlook.com>
2023-07-19 08:26:39 -0600

    Create IconConstants class

---
+++ IconsDemo/IconsDemo/Fonts/IconConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IconsDemo.Fonts
+{
+	public static class IconConstants
+	{
+		public const string Flash = "\ue800";
+		public const string UserAdd = "\ue802";
+		public const string EmoGrin = "\ue80c";
+		public const string EmoAngry = "\ue80d";
+	}
+}
